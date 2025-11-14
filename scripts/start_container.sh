a0852335c1b0866ef39c91c1d08c3581c9d17de8
Jeevan121 <37274845+Jeevan121@users.noreply.github.com>
2025-11-14 16:31:14 +0530

    fixed comments

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull jeevan11/simple-python-flask-app
 
-# Run the Docker image as a containerr
+# Run the Docker image as a container
 docker run -d -p 5000:5000 jeevan11/simple-python-flask-app
